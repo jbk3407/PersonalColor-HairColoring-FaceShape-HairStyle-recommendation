3d6d7df8d9590e09451dff53f290821bb9d5b325
jbk3407 <66096775+jbk3407@users.noreply.github.com>
2022-06-21 14:57:43 +0900

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,14 +3,10 @@ import tkinter
 from tkinter import *
 
 
-
-
-
-
 def nextPage():
     tkinter.Tk().destroy()
     # import page
 
 def prevPage():
     Tk().destroy()
-    import page3
+    import personal_color_3
